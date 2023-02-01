33a029a141c027b98a5e4390bf149c1983198d61
Andrew Persic <persic.andrew@gmail.com>
2023-02-01 15:28:12 +1100

    accomplishments and expansion panel

--- src/components/Expand.vue
+++ src/components/Expand.vue
@@ -1,22 +1,76 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted, ref, type Ref } from 'vue'
 
-let expand = false
+interface Props {
+  label: string,
+  expandedLabel?: string,
+}
+
+defineProps<Props>();
+
+let expanded = ref(false)
+
+const COLLAPSED_HEIGHT = '0px';
+let height = COLLAPSED_HEIGHT;
+
+const expandSlot: Ref<HTMLElement> = ref(null) as unknown as Ref<HTMLElement>
+
+onMounted(() => {
+  expandSlot.value.style.height = COLLAPSED_HEIGHT;
+  window.addEventListener('resize', onResize)
+  onResize()
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', onResize)
+})
+
+function onResize() {
+  height = expandSlot.value.scrollHeight + 'px';
+}
+
+function toggle() {
+  if (expanded.value) {
+    expandSlot.value.style.height = COLLAPSED_HEIGHT;
+  } else {
+    expandSlot.value.style.height = height;
+  }
+  expanded.value = !expanded.value;
+}
 
 </script>
 
 <template>
-  <div class="expand border pb-2" :style="expand ? 'height: auto;' : 'height: 1.1em;'">
-    <div @click="expand = !expand">
-      <slot name="label"></slot>
+  <div @click="toggle" class="expand border pa-2">
+    <div>
+      <div :class="{ arrow: true, rotate: expanded}">▶&nbsp;</div>
+      <div class="label" :style="expandedLabel && expanded ? 'display: none;' : ''">{{ label }}</div>
+      <div class="label" :style="expandedLabel && expanded ? '' : 'display: none;'">{{ expandedLabel }}</div>
     </div>
 
-    <slot></slot>
+    <div class="expand-slot" ref="expandSlot">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
-<style scoped>
-.expand {
-  transition: height 0.5s;
-  overflow: clip;
+<style scoped lang="postcss">
+.arrow {
+  display: inline-block;
+  transition: transform 0.2s;
+  /* transform-origin: center; */
+  transform-origin: 34% 52%;
+  &.rotate {
+    transform: rotate(90deg);
+  }
+}
+
+.label {
+  display: inline;
+}
+
+.expand-slot {
+  transition: height 0.2s ease-out;
+  overflow: hidden;
 }
 </style>
--- src/views/Accomplishments.vue
+++ src/views/Accomplishments.vue
@@ -17,24 +17,30 @@ import Quote from '@/components/Quote.vue'
       <li>Identified pain points in the China region Pages go down without a fallback or being unable to test new designs for canva.com's pages</li>
       <li>Set up communication with China, Conversion Rate Optimisation and Landing Page teams to define goals</li>
       <li>Deployed a proxy service, landing page caching mechanism, and experiments config service in the China region</li>
-      <li>Success metrics: Reduced number of 404s in the China region to near 0 and contributed to conversion rate improvement of 18% that year</li>
+      <li>Success metrics: Reduced number of 404s in the China region to near 0 and contributed to conversion rate improvement of 6% that year</li>
     </ul>
 
-    <Expand v-show="false">
-      <template #label>
-        <span>the title</span>
-      </template>
-      <p>When I arrived at Canva the China region as a whole was somewhat neglected. Unfortunately, this is also Canva's fastest growing customer base, so letting  looked like a huge mistake. The first step to enable these features was to deploy a proxy service in the China region, responsible for looking at each incoming request and either passing it to the page rendering service or serving a cached version. This required setting up a new ECS Fargate deployment for an Nginx instance, refactoring our existing Nginx configuration to support mutliple environments, and creating a scraper to cache pages in an S3 bucket when they </p>
+    <Expand class="ml-4 my-2" label="Read more" expanded-label="Read less">
+      <div class="ml-2">
+        <p>When I arrived at Canva the China region as a whole was somewhat neglected. Unfortunately, this is also Canva's fastest growing customer base, so letting  looked like a huge mistake. To enable these features I had to deploy a proxy service in the China region, responsible for looking at each incoming request and either passing it to the page rendering service or serving a cached version. This entails:
+          <ol>
+            <li>Setting up a new ECS Fargate deployment for an Nginx instance in the CN AWS region</li>
+            <li>Refactoring our existing Nginx configuration to support mutliple environments</li>
+            <li>Creating a scraper and S3 bucket to cache pages so they can be used for fallback</li>
+          </ol>
+        </p>
+      </div>
     </Expand>
 
     <h2>Canva: CMS → Rendering Service Sync</h2>
 
     <ul>
-      <li>Building relationships with content managers revealed that errors are often swallowed when syncing data from the CMS to the landing page rendering service</li>
+      <li>Building relationships with content managers revealed that there is no retry or management process for errors when syncing data from the CMS to the landing page rendering service</li>
       <li>Set up communication with CMS team, SEO and Landing Page teams to define goals</li>
-      <li>Deployed a message queue to contain sync operations between CMS and rendering service. Added error reporting using a dead letter queue.</li>
+      <li>Deployed a message queue to contain sync operations between CMS and rendering service.</li>
+      <li>Added error reporting using a dead letter queue and UI tools to inspect and retry failed sync operations</li>
+      <li>Success metrics: Able to sync millions of rows of landing page data without intervention from engineers if there are conflicts between edits or data consistency issues</li>
     </ul>
-
   </div>
 </template>
 
