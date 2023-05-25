99bb61c1226bd2587bee4507c8518b914bcf6038
Andrew Persic <persic.andrew@gmail.com>
2023-05-25 17:02:20 +0900

    weird paperjs land

--- src/views/DrawMode.vue
+++ src/views/DrawMode.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
   import { onMounted, onUnmounted, reactive, ref } from 'vue'
   import decomp from 'poly-decomp'
-  import Matter, { Common } from 'matter-js'
-  import Line from '@/matter-lines/main'
+  import Matter, { Common, Vector, type Vertex } from 'matter-js'
+  import paper from 'paper'
 
   const STATE_KEY = 'drawModeState'
   const DEBOUNCE_LIMIT = 100
@@ -13,14 +13,7 @@
 
   Common.setDecomp(decomp)
 
-  // Matter.Bodies.decomp = decomp
-  // Matter.Bodies.fromVertices = function(x, y, vertexSets, options, flagInternal, removeCollinear, minimumArea) {
-  //   var decomp: any = decomp
-  //   return Matter.Bodies.fromVertices(x, y, vertexSets, options, flagInternal, removeCollinear, minimumArea)
-  // }
-
-  console.log('decomp', Common.getDecomp())
-  // console.log('decomp', Matter.Bodies.decomp)
+  const scope = new paper.PaperScope()
 
   // module aliases
   const Engine = Matter.Engine,
@@ -34,7 +27,7 @@
 
   let render: Matter.Render
 
-  let line: Line
+  let mousePath: paper.Path
 
   // create two boxes and a ground
   const boxA = Bodies.rectangle(400, 200, 80, 80);
@@ -56,18 +49,73 @@
   function draw(e: MouseEvent) {
     const time = Date.now()
     if (isDrawing.value && e.target === render.canvas && time - timeOfLastPoint > DEBOUNCE_LIMIT) {
-      // console.log(render.mouse.position)
-      line.addPoint(render.mouse.position)
-      timeOfLastPoint = time
+      // console.log(e)
     }
       
   }
+
   function stopDrawing(e: MouseEvent) {
     isDrawing.value = false
   }
+
   function startDrawing(e: MouseEvent) {
-    isDrawing.value = true
-    line = new Line(engine.world, [render.mouse.position])
+    // isDrawing.value = true
+    let isDrawing = false
+    scope.activate()
+    const tool = new paper.Tool();
+    tool.onMouseDown = (event: paper.ToolEvent) => {
+      isDrawing = true;
+      mousePath = new paper.Path({
+      strokeColor: '#E4141B',
+      strokeWidth: 20,
+      strokeCap: 'round'
+    });
+    }
+    tool.onMouseDrag = (event: paper.ToolEvent) => {
+      // event.delta.angle += 90;
+      // event.delta.length = 4;
+
+      // mousePath.add(new paper.Point(event.middlePoint.x + event.delta.x / 2, event.middlePoint.y + event.delta.y / 2));
+      // mousePath.insert(0, new paper.Point(event.middlePoint.x - event.delta.x / 2, event.middlePoint.y - event.delta.y / 2));
+      if (isDrawing) {
+        mousePath.add(event.point)
+        let step = event.delta.divide(2);
+        step.angle += 90;
+        step.length = 4;
+
+        let top = event.middlePoint.add(step);
+        let bottom = event.middlePoint.subtract(step);
+
+        mousePath.add(top);
+        mousePath.insert(0, bottom);
+      }
+    }
+    tool.onMouseUp = (event: paper.ToolEvent) => {
+      mousePath.add(event.point);
+      mousePath.closed = true;
+      isDrawing = false
+
+      const vertices: Vector[] = mousePath.segments.map(s => s.point)
+
+      const object = Bodies.fromVertices(Matter.Vertices.centre(vertices).x, Matter.Vertices.centre(vertices).y, [vertices], {
+          isStatic: true,
+          render: {
+              fillStyle: 'red',
+              strokeStyle: 'red'
+          }
+      }, true);
+      Matter.World.add(engine.world, object);
+
+      function boundsCenter(minX: number, minY: number, maxX: number, maxY: number){
+        return new paper.Point((maxX + minX)/2, (maxY + minY)/2);
+      }
+      const difference = mousePath.position.subtract(boundsCenter(object.bounds.min.x, object.bounds.min.y, object.bounds.max.x, object.bounds.max.y));
+      // align matterjs object with paperjs drawn shape
+      Matter.Body.translate(object, difference);
+    }
+
+    // line = new Line(engine.world, [render.mouse.position])
+    
   }
 
   onMounted(() => {
@@ -83,6 +131,14 @@
         options: {
           // showMousePosition: true,
         },
+    });
+
+    scope.setup(render.canvas)
+
+    mousePath = new paper.Path({
+      strokeColor: '#E4141B',
+      strokeWidth: 20,
+      strokeCap: 'round'
     });
 
     // add a mouse
