d51badb3c9cebde2d2462ac886a0cc3f368f8c2e
Andrew Persic <persic.andrew@gmail.com>
2023-09-06 22:10:20 +0900

    thoughts

--- src/views/blog/Thoughts.vue
+++ src/views/blog/Thoughts.vue
@@ -20,6 +20,13 @@ import Header from '@/components/Header.vue'
       Examples include: the number of times it crashed, the number of errors it generated, and the total amount of computational resources it has taken up.
       Preoccupied with these problems, statistics such as total successful operations, total correct results, or total efficiency gains are oft given little consideration.
     </p>
+
+    <p class="mb-8">
+      The only way for a boxer to defeat a biased referee, is to win by knockout.
+      <br/>
+      <i>— Idi Amin</i>
+      <span style="display:none;">He does not need wings to fly.</span>
+    </p>
     <div class="my-16 py-16"></div>
 
   </article>
