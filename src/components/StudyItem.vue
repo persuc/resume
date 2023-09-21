23e18382425677c1a63e8bbeb8b2b1eaf92a1b87
Andrew Persic <persic.andrew@gmail.com>
2023-09-21 15:04:39 +0200

    Update study items styling

---
+++ src/components/StudyItem.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface Props {
+  title: string,
+  link: string,
+  description?: string,
+  category: string,
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex mb-2">
+    <div class="flex-1 mr-4">
+      <div>
+        <a class="font-mono" target="_blank" :href="link">{{ title }}</a>
+        <div class="font-mono sm:hidden my-1">
+          <span class="rounded-full bg-gray-100 px-2 py-0.5">
+            {{ category }}
+          </span>
+        </div>
+        <div v-if="description" v-html="description" />
+        <slot></slot>
+      </div>
+    </div>
+    <div class="w-64 font-mono hidden sm:inline">
+      <span class="rounded-full bg-gray-100 px-2 py-0.5">
+        {{ category }}
+      </span>
+    </div>
+  </div>
+</template>
+
--- src/views/Study.vue
+++ src/views/Study.vue
@@ -2,8 +2,109 @@
 import Header from '@/components/Header.vue'
 import TrafficLight from '@/components/TrafficLight.vue'
 import friendSongTimelineImage from '@/assets/FriendSongTimeline.png'
+import StudyItem from '@/components/StudyItem.vue'
 
 type Difficulty = 'easy' | 'medium' | 'hard'
+
+type Resource = {
+  link: string
+  title: string
+  description: string
+  category: string
+}
+
+const resources: Record<string, Resource[]> = {
+  "Frontend Web": [
+    {
+      link: "https://flexboxfroggy.com/",
+      title: "Flexbox Froggy",
+      description: "Game for learning how to make website layouts with CSS flexbox",
+      category: "UI / webpage layout",
+    },
+    {
+      link: "https://www.w3schools.com/js/default.asp",
+      title: "W3 Schools JS tutorial",
+      description: "Beginner resource for JavaScript (web) programming",
+      category: "Web programming",
+    },
+    {
+      link: "https://www.11ty.dev/docs/getting-started/",
+      title: "11ty.js",
+      description: "A framework for building static websites. It might be fun to follow their getting started tutorial and make your own blog!",
+      category: "Frameworks / Personal Site",
+    },
+    {
+      link: "https://legacy.reactjs.org/tutorial/tutorial.html",
+      title: "React.js",
+      description: "A more advanced framework for building static websites. It might be fun to follow their getting started tutorial and make your own blog!",
+      category: "Frameworks / Personal Site",
+    },
+    {
+      link: "https://developers.cloudflare.com/pages/framework-guides/deploy-anything/",
+      title: "Cloudflare Pages",
+      description: "Once you have your first website, you can use this to make it available on the public internet",
+      category: "Frameworks / Personal Site",
+    },
+  ],
+  "Algorithms & Data Structures": [
+    {
+      link: "https://drive.google.com/file/d/1C_3Wbdi2T_PbJxckv90IDEjIBJDdgiBs/view?usp=sharing",
+      title: "Algorithms & Data Structures Notes",
+      description: "Algorithms & data structures are useful in the daily life of any programmer, and also for passing interviews. Usually FAANG companies focus on these, and knowing them helps understand how to write performant code, and how computers work internally",
+      category: "Notes",
+    },
+    {
+      link: "https://leetcode.com/explore/featured/card/the-leetcode-beginners-guide/",
+      title: "Leetcode.com",
+      description: "A site dedicated to practice questions for algorithms & data structures. Many big companies use these questions.",
+      category: "Practical Exercises",
+    },
+    {
+      link: "https://www.teamblind.com/post/New-Year-Gift---Curated-List-of-Top-75-LeetCode-Questions-to-Save-Your-Time-OaM1orEU",
+      title: "Blind75",
+      description: "A curated list of commonly asked LeetCode questions.",
+      category: "LeetCode compendium",
+    },
+    {
+      link: "https://www.techinterviewhandbook.org/grind75",
+      title: "Grind75",
+      description: "A curated list of commonly asked LeetCode questions.",
+      category: "LeetCode compendium",
+    },
+  ],
+  "System Design and Computer Architecture": [
+    {
+      link: "https://drive.google.com/file/d/1rTt0WPdLmhctQIDHVG4mlWHbdLM4fGPd/view?usp=sharing",
+      title: "Designing Data Intensive Applications by Marting Kleppman",
+      description: "Amazing resource to understand databases. Advanced text, so I can walk you through it if you like",
+      category: "Databases",
+    },
+    {
+      link: "https://en.algorithmica.org/",
+      title: "Algorithmica.org",
+      description: "Deep analyses of how computer programs are executed. Useful to understand how to achieve good performance, and understand computer architecture. Also an advanced text",
+      category: "Compilers & Performance",
+    },
+    {
+      link: "https://www.youtube.com/channel/UCbvDQKzAJ5GwCjTrv4FWkxg",
+      title: "Jordan has no life",
+      description: "Great YouTube channel for system design and databases content",
+      category: "System Design",
+    },
+    {
+      link: "https://www.youtube.com/@SystemDesignInterview",
+      title: "System Design Interview",
+      description: "Great YouTube channel for system design content",
+      category: "System Design",
+    },
+    {
+      link: "https://github.com/dendibakh/perf-ninja",
+      title: "Perf Ninja",
+      description: "A series of take-home exercises for learning performance programming in <code>C++</code>",
+      category: "Compilers & Performance",
+    },
+  ]
+}
 
 const leetcodes: {
   link: string,
@@ -13,7 +114,7 @@ const leetcodes: {
   { link: "https://leetcode.com/problems/number-of-recent-calls/", title: "Number of recent calls", difficulty: "easy" },
   { link: "https://leetcode.com/problems/design-hashmap/", title: "Design hashmap", difficulty: "easy" },
   { link: "https://leetcode.com/problems/ransom-note/", title: "Ransom note", difficulty: "easy" },
-  { link: "https://leetcode.com/problems/total-distance-traveled/", title: "Total distance traveled", difficulty: "easy" },
+  { link: "https://leetcode.com/problems/total-distance-traveled/", title: "Total distance travelled", difficulty: "easy" },
   { link: "https://leetcode.com/problems/binary-search-tree-iterator/", title: "Binary search tree iterator", difficulty: "medium" },
   { link: "https://leetcode.com/problems/linked-list-cycle/", title: "Linked list cycle", difficulty: "medium" },
   { link: "https://leetcode.com/problems/design-a-stack-with-increment-operation/", title: "Design a stack with increment operation", difficulty: "medium" },
@@ -54,126 +155,34 @@ const difficultyColor: Record<Difficulty, string> = {
 
     <p>Don't feel overwhelmed, these are just some resources to read through to get a feel for the different topics you can go deeper into. If you find something you like, let me know and I can tailor a study plan for you and explain what the field is about a little. Don't try to master everything at once, hehe. Also the best way to learn is by practical example, so if you want to do some particular project (chrome extension, discord bot, blog website are all good first-project choices) I can walk you through and help you where you get stuck. That way it's not all just boring theory.</p>
 
-    <div class="font-mono grid gap-4" style="grid-template-columns: auto 16rem;">
-      <div><p class="text-xl mt-8 font-sans">Frontend Web</p></div>
-      <div></div>
+    <template v-for="resourceCategory in Object.entries(resources)">
+      <p class="text-xl mt-8 font-sans">{{ resourceCategory[0] }}</p>
+      <StudyItem v-for="resource in resourceCategory[1]" :link="resource.link" :description="resource.description" :category="resource.category" :title="resource.title" />
+    </template>
+      
+    <p class="text-xl mt-8 font-sans">My Practice Questions</p>
+    <p>Questions I have encountered myself, or come up with to test the concepts covered above</p>
 
-      <div>
-        <a target="_blank" href="https://flexboxfroggy.com/">Flexbox Froggy</a>
-        <p>Game for learning how to make website layouts with CSS flexbox</p>
-      </div>
-      <div>UI / webpage layout</div>
-      <div>
-        <a target="_blank" href="https://www.w3schools.com/js/default.asp">W3 Schools JS tutorial</a>
-        <p>Beginner resource for JavaScript (web) programming</p>
-      </div>
-      <div>Web programming</div>
-      <div>
-        <a target="_blank" href="https://www.11ty.dev/docs/getting-started/">11ty.js</a>
-        <p>A framework for building static websites. It might be fun to follow their getting started tutorial and make your own blog!</p>
-      </div>
-      <div>Frameworks / Personal Site</div>
-      <div>
-        <a target="_blank" href="https://legacy.reactjs.org/tutorial/tutorial.html">React.js</a>
-        <p>A more advanced framework for building static websites. It might be fun to follow their getting started tutorial and make your own blog!</p>
-      </div>
-      <div>Frameworks / Personal Site</div>
-      <div>
-        <a target="_blank" href="https://developers.cloudflare.com/pages/framework-guides/deploy-anything/">Cloudflare Pages</a>
-        <p>Once you have your first website, you can use this to make it available on the public internet</p>
-      </div>
-      <div>Frameworks / Personal Site</div>
-
-      <div><p class="text-xl mt-8 font-sans">Algorithms &amp; Data Structures</p></div>
-      <div></div>
-
-      <div>
-        <a target="_blank" href="https://drive.google.com/file/d/1C_3Wbdi2T_PbJxckv90IDEjIBJDdgiBs/view?usp=sharing">Algorithms &amp; Data Structures Notes</a>
-        <p>Algorithms &amp; data structures are useful in the daily life of any programmer, and also for passing interviews. Usually FAANG companies focus on these, and knowing them helps understand how to write performant code, and how computers work internally</p>
-      </div>
-      <div>Notes</div>
-      <div>
-        <a target="_blank" href="https://leetcode.com/explore/featured/card/the-leetcode-beginners-guide/">Leetcode.com</a>
-        <p>A site dedicated to practice questions for algorithms &amp; data structures. Many big companies use these questions.</p>
-      </div>
-      <div>Practical Exercises</div>
-      <div>
-        <a target="_blank" href="https://www.teamblind.com/post/New-Year-Gift---Curated-List-of-Top-75-LeetCode-Questions-to-Save-Your-Time-OaM1orEU">Blind75</a>
-        <p>A curated list of commonly asked LeetCode questions.</p>
-      </div>
-      <div>LeetCode compendium</div>
-      <div>
-        <a target="_blank" href="https://www.techinterviewhandbook.org/grind75">Grind75</a>
-        <p>A curated list of commonly asked LeetCode questions.</p>
-      </div>
-      <div>LeetCode compendium</div>
-
-      <div><p class="text-xl mt-8 font-sans">System Design and Computer Architecture</p></div>
-      <div></div>
-
-      <div>
-        <a target="_blank" href="https://drive.google.com/file/d/1rTt0WPdLmhctQIDHVG4mlWHbdLM4fGPd/view?usp=sharing">Designing Data Intensive Applications by Marting Kleppman</a>
-        <p>Amazing resource to understand databases. Advanced text, so I can walk you through it if you like</p>
-      </div>
-      <div>Databases</div>
-      <div>
-        <a target="_blank" href="https://en.algorithmica.org/">Algorithmica.org</a>
-        <p>Deep analyses of how computer programs are executed. Useful to understand how to achieve good performance, and understand computer architecture. Also an advanced text</p>
-      </div>
-      <div>Compilers &amp; Performance</div>
-      <div>
-        <a target="_blank" href="https://www.youtube.com/channel/UCbvDQKzAJ5GwCjTrv4FWkxg">Jordan has no life</a>
-        <p>Great YouTube channel for system design and databases content</p>
-      </div>
-      <div>System Design</div>
-      <div>
-        <a target="_blank" href="https://www.youtube.com/@SystemDesignInterview">System Design Interview</a>
-        <p>Great YouTube channel for system design content</p>
-      </div>
-      <div>System Design</div>
-      <div>
-        <a target="_blank" href="https://github.com/dendibakh/perf-ninja">Perf Ninja</a>
-        <p>A series of take-home exercises for learning performance programming in <code>C++</code></p>
-      </div>
-      <div>Compilers &amp; Performance</div>
-
-      <div>
-        <p class="text-xl mt-8 font-sans">My Practice Questions</p>
-        <p>Questions I have encountered myself, or come up with to test the concepts covered above</p>
-      </div>
-      <div></div>
-
-      <div>
-        <a target="_blank" href="https://app.coderpad.io/PQQXDRQX">Custom: Implement Reactivity</a>
-        <p>Learn how modern web frameworks like React.js and Vue.js work by implementing reactivity yourself!</p>
-        <pre v-highlightjs><code class="javascript rounded">function reactive()
+    <StudyItem link="https://app.coderpad.io/PQQXDRQX" category="Frontend - 60 mins" title="Implement Reactivity">
+      <p>Learn how modern web frameworks like React.js and Vue.js work by implementing reactivity yourself!</p>
+      <pre v-highlightjs><code class="javascript rounded">function reactive()
 function watch()
 function ref()
 function computed()
 function render()</code></pre>
-      </div>
-      <div>Frontend - 60 mins</div>
+    </StudyItem>
 
-      <div>
-        <a target="_blank" href="https://app.coderpad.io/PQQXDRQX">Spotify: Traffic Light Component</a>
-        <p>Design a toy component as below to display a traffic light (in plain JavaScript)</p>
-        <TrafficLight />
-      </div>
-      <div>Frontend - 60 mins</div>
-
-      <div>
-        <a target="_blank" href="https://app.coderpad.io/PQQXDRQX">Spotify: Design Friends Timeline</a>
-        <p>Systems design question for a feed of your friends' most recently listened songs</p>
-        <img :src="friendSongTimelineImage" class="h-80" />
-      </div>
-      <div>System Design - 90 mins</div>
-
-      <div>
-        <a target="_blank" href="https://app.coderpad.io/PQQXDRQX">Microsoft: E-Tag Middleware</a>
-        <p>Design node.js middleware to cache resources using <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/ETag">E-Tags</a></p>
-      </div>
-      <div>Web Backend - 60 mins</div>
-    </div>
+    <StudyItem link="https://app.coderpad.io/PQQXDRQX" category="Frontend - 60 mins" title="Traffic Light Component">
+      <p>Design a toy component as below to display a traffic light (in plain JavaScript)</p>
+      <TrafficLight />
+    </StudyItem>
+    <StudyItem link="https://app.coderpad.io/PQQXDRQX" category="System Design - 90 mins" title="Design Friends Timeline">
+      <p>Systems design question for a feed of your friends' most recently listened songs</p>
+      <img :src="friendSongTimelineImage" class="h-80" />
+    </StudyItem>
+    <StudyItem link="https://app.coderpad.io/PQQXDRQX" category="Web Backend - 60 mins" title="E-Tag Middleware">
+      <p>Design node.js middleware to cache resources using <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/ETag">E-Tags</a></p>
+    </StudyItem>
 
     <p class="text-xl mt-8 font-sans">My LeetCode Questions</p>
     <p>I have run these questions with other people in the past. LeetCode questions are more suited for backend interviews, and these were selected particularly because they suit the format of a backend interview.</p>
