26b686949179fd276604f511accc05914da4c3b9
Andrew Persic <persic.andrew@gmail.com>
2023-09-27 13:50:08 +0200

    Fix Component types

--- src/components/Component.vue
+++ src/components/Component.vue
@@ -1,49 +1,59 @@
 <script setup lang="ts">
-import { reactive, ref, type ComponentInternalInstance, getCurrentInstance } from 'vue'
+import type { FunctionalComponent, Renderer } from 'vue'
+import { reactive, type ComponentInternalInstance } from 'vue'
 
 interface Props {
-  // component: ComponentInternalInstance,
-  component: any
+  component: ComponentInternalInstance & FunctionalComponent & Renderer,
   defaults: Record<string, string | boolean | number>
   classes?: string
 }
+type TypeName = 'Boolean' | 'String' | 'Number' | 'Array'
+interface TypeFunction extends Function {
+    name: TypeName;
+}
+type BasicType = string | boolean | number
+type PropType = BasicType | BasicType[]
 
 const props = defineProps<Props>()
+const componentProps = Object.entries(
+  props.component.props ?? {}
+) as [string, { type: TypeFunction, required: boolean }][]
 
-const componentRef = ref(null)
-
-const typeToDefaultValue: Record<string, any> = {
+const typeToDefaultValue: Record<TypeName, PropType> = {
   'Boolean': false,
   'String': '',
   'Number': 0,
   'Array': []
 }
 
-const boundProps = reactive(Object.entries((props.component.props ?? {})).reduce((acc, val) => ({
-  ...acc,
-  [val[0]]: props.defaults[val[0]] !== undefined ? props.defaults[val[0]] : typeToDefaultValue[val[1].type.name]
-}), {}))
-
-const name = props.component.__file.match(/.*\/([A-z]+)\.vue/)[1]
-
-const slots: string[] = []
-for (const s of props.component.render.toString().matchAll(/_renderSlot\(.*, "(.*)"/g)) {
-  slots.push(s[1])
+const typeNameToInputType: Record<string, string> = {
+  Boolean: 'checkbox',
+  String: 'text',
+  Number: 'number',
 }
+
+const boundProps = reactive(
+  componentProps.reduce((acc, val) => ({
+      ...acc,
+      [val[0]]: props.defaults[val[0]] !== undefined ? props.defaults[val[0]] : typeToDefaultValue[val[1].type.name]
+    }), {} as Record<string, PropType>)
+  )
+
+const name = props.component.__file?.match(/.*\/([A-z]+)\.vue/)?.[1]
+
+const slots = Array.from(
+  props.component.render.toString().matchAll(/_renderSlot\(.*, "(.*)"/g)
+).map(match => match[1])
 
 </script>
 
 <template>
   <div class="border border-gray-200 rounded p-4">
     <p class="text-lg">{{ name }}</p>
-    <div v-for="prop in Object.entries(component.props ?? {})" class="inline-block mr-2 bg-slate-200 p-2 rounded whitespace-nowrap my-1">
+    <div v-for="prop in componentProps" class="inline-block mr-2 bg-slate-200 p-2 rounded whitespace-nowrap my-1">
       {{ prop[0] }}
       <input
-        :type="{
-          Boolean: 'checkbox',
-          String: 'text',
-          Number: 'number',
-        }[prop[1].type.name]" v-model="boundProps[prop[0]]"
+        :type="typeNameToInputType[prop[1].type.name as string]" v-model="boundProps[prop[0]]"
         :class="`${prop[1].type.name === 'String' ? 'border-b' : ''} pl-1`"
        />
     </div>
