008d64527136ae12a42bc489fcc689e912b9d081
Andrew Persic <persic.andrew@gmail.com>
2024-02-14 17:23:22 +0000

    Fix previewcard for wrapped lines

--- src/components/PreviewCard.vue
+++ src/components/PreviewCard.vue
@@ -14,7 +14,7 @@ withDefaults(defineProps<Props>(), {
 <template>
   <a :href="card.path" :class="`!no-underline ${boundHeight ? 'h-56' : ''} w-64`">
     <div :class="`p-4 border rounded flex flex-col items-center ${boundHeight ? 'h-56' : ''} w-64`">
-      <span class="underline leading-3">{{ card.title }}</span>
+      <span class="underline">{{ card.title }}</span>
       <component v-if="card.previewComponent" :is="card.previewComponent"
         class="flex mt-4 text-black h-full w-full bg-gray-50 px-4 p-2 rounded" />
       <img v-else-if="card.thumb" :src="card.thumb" class="pt-4 object-contain w-full h-full" />
