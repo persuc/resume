d5ea0a1f8e1d5d3347d000807ab199f434c76af0
Andrew Persic <persic.andrew@gmail.com>
2024-02-24 16:25:01 +0000

    home chrome/safari overflow

--- src/components/PreviewCard.vue
+++ src/components/PreviewCard.vue
@@ -13,11 +13,13 @@ withDefaults(defineProps<Props>(), {
 
 <template>
   <a :href="card.path" :class="`!no-underline ${boundHeight ? 'h-56' : ''} w-64`">
-    <div :class="`p-4 border rounded flex flex-col items-center ${boundHeight ? 'h-56' : ''} w-64`">
-      <span class="underline">{{ card.title }}</span>
-      <component v-if="card.previewComponent" :is="card.previewComponent"
-        class="flex mt-4 text-black h-full w-full bg-gray-50 px-4 p-2 rounded" />
-      <img v-else-if="card.thumb" :src="card.thumb" class="pt-4 object-contain w-full h-full" />
+    <div :class="`p-4 border rounded flex flex-col items-center gap-y-2 ${boundHeight ? 'h-56' : ''} w-64`">
+      <div class="underline">{{ card.title }}</div>
+      <div v-if="card.previewComponent || card.thumb" class="w-full h-full overflow-auto">
+        <component v-if="card.previewComponent" :is="card.previewComponent"
+          class="flex mt-4 text-black h-full w-full bg-gray-50 px-4 p-2 rounded" />
+        <img v-else-if="card.thumb" :src="card.thumb" class="object-contain h-full w-full" />
+      </div>
     </div>
   </a>
 </template>
