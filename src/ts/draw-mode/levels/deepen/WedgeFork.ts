2c2ece95968b57969f206d6b458f0a69ce2eb597
Andrew Persic <persic.andrew@gmail.com>
2024-04-26 14:22:14 +0100

    finalise wedgefork

--- src/ts/draw-mode/levels/deepen/WedgeFork.ts
+++ src/ts/draw-mode/levels/deepen/WedgeFork.ts
@@ -8,20 +8,20 @@ import { type Level } from "@/ts/draw-mode/Level"
 const level: LevelSpec = {
   generateBodies(engine: Engine, level: Level, onEnd: () => any) {
 
-    const walls = BodyUtil.wallFloor()
+    const walls = BodyUtil.wallCup()
 
-    const ball = Bodies.circle(400, 220, 40)
+    const ball = Bodies.circle(400, 150, 40)
 
-    const leftWedge = BodyUtil.isosceles(340, 395, 100, 555, {
+    const leftWedge = BodyUtil.isosceles(340, 295, 100, 555, {
       isStatic: true
     })
 
-    const rightWedge = BodyUtil.isosceles(460, 395, 100, 555, {
+    const rightWedge = BodyUtil.isosceles(460, 295, 100, 555, {
       isStatic: true
     })
 
     const target = {
-      body: Bodies.rectangle(400, 80, 800, 160, {
+      body: Bodies.rectangle(400, 40, 100, 100, {
         isStatic: true,
         collisionFilter: {
           mask: 0,
@@ -31,7 +31,7 @@ const level: LevelSpec = {
       opacity: 0.6
     }
 
-    LevelEvent.onCondition(engine, () => ball.position.y < 200, onEnd)
+    LevelEvent.onCondition(engine, () => ball.position.y < 120, onEnd)
 
     return [
       walls,
@@ -42,7 +42,7 @@ const level: LevelSpec = {
     ]
   },
   id: 'WedgeFork',
-  text: "<span>Lift the ball into the target area.</span><div style=\"\" >Warning! This level is unsolved. If you are the first to solve it, please <a target=\"_blank\" style=\"pointer-events: all\" href=\"https://github.com/andrew-p-dev/resume/issues\">raise an issue</a> to let me know! (You get your name here)</div>"
+  text: "<span>Lift the ball into the target area.</div>"
 }
 
 export default level
