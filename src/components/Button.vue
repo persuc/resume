15faf5b85249f72f11a55778371a7778061360e6
Andrew Persic <persic.andrew@gmail.com>
2025-08-21 22:19:01 +0100

    outline button initial

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,22 +1,5 @@
-<template>
-  <div :class="[
-    'button flex items-center h-10 w-fit focus:outline-none select-none',
-    variant,
-    variant === 'primary' ? 'bg-gray-800 text-white hover:bg-gray-700' : '',
-    variant === 'secondary' ? 'bg-gray-200 text-gray-800 hover:bg-gray-300' : '',
-    variant === 'text' ? 'px-4' : 'rhomboid mx-6 px-2',
-    variant === 'outline' ? 'outline-none before:border-2 before:border-gray-800' : '',
-    disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer',
-    disabled && (variant === 'primary') ? 'hover:bg-gray-800' : '',
-    disabled && (variant === 'secondary') ? 'hover:bg-gray-200' : '',
-  ]" :aria-disabled="disabled" :tabindex="disabled ? -1 : 0" @keydown.enter="handleKeyDown"
-    @keydown.space="handleKeyDown">
-    <slot></slot>
-  </div>
-</template>
-
 <script setup lang="ts">
-type ButtonVariant = 'primary' | 'secondary' | 'text' | 'outline'
+type ButtonVariant = 'primary' | 'secondary' | 'outline' | 'text'
 
 interface Props {
   variant?: ButtonVariant
@@ -35,3 +18,20 @@ const handleKeyDown = (event: KeyboardEvent) => {
   }
 }
 </script>
+
+<template>
+  <div :class="[
+    'button flex items-center h-10 w-fit focus:outline-none select-none',
+    variant,
+    variant === 'primary' ? 'rhomboid mx-6 px-2 bg-gray-800 text-white hover:bg-gray-700' : '',
+    variant === 'secondary' ? 'rhomboid mx-6 px-2 bg-gray-200 text-gray-800 hover:bg-gray-300' : '',
+    variant === 'outline' ? 'rhomboid outline mx-6 px-2 bg-transparent text-gray-800 hover:text-gray-950' : '',
+    variant === 'text' ? 'px-4' : '',
+    disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer',
+    disabled && (variant === 'primary') ? 'hover:bg-gray-800' : '',
+    disabled && (variant === 'secondary') ? 'hover:bg-gray-200' : '',
+  ]" :aria-disabled="disabled" :tabindex="disabled ? -1 : 0" @keydown.enter="handleKeyDown"
+    @keydown.space="handleKeyDown">
+    <slot></slot>
+  </div>
+</template>
